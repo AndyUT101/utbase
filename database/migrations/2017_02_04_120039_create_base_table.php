20a40ba858359133ec3dde5f8580caa8de4a643a
worminfo <worminfo@gmail.com>
2017-02-05 00:52:16 +0800

    database init

--- database/migrations/2017_02_04_120039_create_base_table.php
+++ database/migrations/2017_02_04_120039_create_base_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBaseTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('presetusers', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('nickname')->unique();
+            $table->char('gender', 1);
+            $table->string('email', 96)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -30,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('presetusers');
+        Schema::dropIfExists('user');
     }
 }
