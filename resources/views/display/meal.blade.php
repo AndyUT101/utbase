98acdcc3a8bcf7062b6e93e5a8c85a19a4296dd6
worminfo <worminfo@gmail.com>
2017-03-10 00:08:35 +0800

    bugfix2

--- resources/views/display/meal.blade.php
+++ resources/views/display/meal.blade.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>meal</title>
-<script src="{{ url('js/jquery-1.10.2.min.js"></script>
+<script src="{{ url('js/jquery-1.10.2.min.js') }}"></script>
 <script src="{{ url('js/flowtype.js') }}"></script>
 <script src="{{ url('js/jquery.textfill.min.js') }}"></script>
 <style>
--- resources/views/display/timeweather.blade.php
+++ resources/views/display/timeweather.blade.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>timeweather</title>
-<script src="{{ url('js/jquery-1.10.2.min.js"></script>
+<script src="{{ url('js/jquery-1.10.2.min.js') }}"></script>
 <script src="{{ url('js/jquery.marquee.min.js') }}"></script>
 <style>
 
