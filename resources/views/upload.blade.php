7a03204bed58a865b33bca9c366336df232545f7
worminfo <worminfo@gmail.com>
2017-03-09 21:24:22 +0800

    View url update

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -10,6 +10,23 @@
 </div>
 @endif
 <div class="row">
+<div class="col-md-12">
+    <div class="panel panel-cascade">
+      <div class="panel-heading">
+        <h3 class="panel-title text-primary">
+                        上傳餐單資料
+                        <span class="pull-right">
+                          <a href="#" class="panel-minimize"><i class="fa fa-chevron-up"></i></a>
+                          <a href="#" class="panel-close"><i class="fa fa-times"></i></a>
+                        </span>
+                      </h3>
+      </div>
+      <div class="panel-body panel-border">
+        上傳附餐單資料的xlsx檔案，請在<a href="{{ url('/resource/template') }}" target="_blank">此處下載範本文件</a>。
+      </div>
+      <!-- /panel body -->
+    </div>
+  </div>
   <div class="col-md-12">
     <div class="panel panel-cascade panel-file-upload">
       <div class="panel-heading">
