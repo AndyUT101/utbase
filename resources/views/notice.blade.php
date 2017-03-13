0d03a02afadc37f25298c27c91eafd8b589f98e7
worminfo <worminfo@gmail.com>
2017-03-14 03:09:37 +0800

    Zz

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -6,24 +6,47 @@
     <div class="panel">
       <div class="panel-heading text-primary">
         <h3 class="panel-title"><i class="fa fa-th"></i> 顯示情況
-          <span class="pull-right">
-            <div class="btn-group code">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Classes used"><i class="fa fa-code"></i></a>
-              <ul class="dropdown-menu pull-right list-group" role="menu">
-                <li class="list-group-item"><code>.table</code></li>
-              </ul>
-            </div>
-            <a href="#" class="panel-minimize"><i class="fa fa-chevron-up"></i></a>
-            <a href="#" class="panel-close"><i class="fa fa-times"></i></a>
-          </span>
         </h3>
       </div>
-      <div class="panel-body ">
-        
-      </div>
+<div class="panel-body ">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>資訊內容</th>
+              <th>尺寸</th>
+              <th>瀏覽畫面內容</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>新聞資訊feed</td>
+              <td>1920 x 126</td>
+              <td>{{ url('display/newsfeed') }}</td>
+            </tr>
+            <tr>
+              <td>餐牌顯示</td>
+              <td>1488 x 954</td>
+              <td>{{ url('display/meal') }}</td>
+            </tr>          
+            <tr>
+              <td>日期及時間顯示</td>
+              <td>438 x 954</td>
+              <td>{{ url('display/dateweather') }}</td>
+            </tr>  
+           </tbody>
+        </table>
       <!-- /panel body -->
     </div>
   </div>
-
+<div class="col-md-12">
+    <div class="panel">
+      <div class="panel-heading text-primary">
+        <h3 class="panel-title"><i class="fa fa-th"></i> 顯示位置設定
+        </h3>
+      </div>
+    <div class="panel-body ">
+        <p>使用 <a href="https://www.yodeck.com">YoDeck - </a></p>
+    </div>
+  </div>
 </div>
 @endsection
